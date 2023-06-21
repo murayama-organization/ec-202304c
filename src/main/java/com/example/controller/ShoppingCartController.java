335f63e44252726ac3f3cc1d35a2c10dea2484fc
tkeshun <shun9646@gmail.com>
2023-06-21 16:23:51 +0900

    WIP:カートの機能

--- src/main/java/com/example/controller/ShoppingCartController.java
+++ src/main/java/com/example/controller/ShoppingCartController.java
@@ -1,17 +1,52 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.OrderItem;
+import com.example.domain.OrderTopping;
+import com.example.domain.User;
 import com.example.form.AddItemToShoppingCartForm;
+import com.example.service.ShoppingCartService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/shopping-cart")
 public class ShoppingCartController {
+	@Autowired
+	private HttpSession httpSession;
+
+	@Autowired
+	public ShoppingCartService cartService;
+
+	@GetMapping("/show-cart")
+	public String showCart() {
+		User user = (User) httpSession.getAttribute("currentUser");
+		Integer accessId = null;
+		if (user == null) {
+			accessId = httpSession.getId().hashCode();
+		} else {
+			accessId = user.getId();
+		}
+		List<OrderItem> showShoppingCartContents = cartService.showShoppingCartContents(accessId);
+		for (OrderItem item : showShoppingCartContents) {
+			item.setItem();
+			List<OrderTopping> orderToppingList = item.getOrderToppingList();
+			for (OrderTopping orderTopping : orderToppingList) {
+
+			}
+		}
+		return "ec/cart_list";
+	}
 
 	@GetMapping("/add-item")
-	public String add(AddItemToShoppingCartForm form) {
+	public String addItem(AddItemToShoppingCartForm form) {
 		return null;
 	}
+
 }
--- src/main/java/com/example/service/ShoppingCartService.java
+++ src/main/java/com/example/service/ShoppingCartService.java
@@ -1,5 +1,33 @@
 package com.example.service;
 
-public class ShoppingCartService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Order;
+import com.example.domain.OrderItem;
+import com.example.repository.OrderRepository;
+
+@Service
+@Transactional
+public class ShoppingCartService {
+	private final Integer STATUS = 0;// 注文前
+	@Autowired
+	private OrderRepository orderRepository;
+
+	public List<OrderItem> showShoppingCartContents(Integer sessionOrUserId) {
+		Order order = orderRepository.findByUserIdAndStatus(sessionOrUserId, STATUS);
+		List<OrderItem> cartContent = order.getOrderItemList();
+		return cartContent;
+	}
+
+	public void addItemToShoppingCart() {
+
+	}
+
+	public void removeItemFromShoppingCart(Integer orderItemId) {
+
+	}
 }
