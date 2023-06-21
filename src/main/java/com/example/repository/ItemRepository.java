0e41731944e14d6f70224c670be490bca5cc3218
uedamiku <krn3ancd9@gmail.com>
2023-06-21 09:40:19 +0900

    add: 商品一覧から名前であいまい検索をするリポジトリのメソッドを追加しました。

--- src/main/java/com/example/repository/ItemRepository.java
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -5,21 +5,49 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.example.domain.Item;
 
+/**
+ * itemsテーブルを操作するリポジトリ.
+ * 
+ * @author miku.ueda
+ *
+ */
 public class ItemRepository {
+
+	/**
+	 * Itemオブジェクトを生成するローマッパー.
+	 */
 	private static final RowMapper<Item> ITEM_ROW_MAPPER = new BeanPropertyRowMapper<>(Item.class);
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
+	/**
+	 * 商品一覧情報を値段の安い順で取得します.
+	 * 
+	 * @return 全商品一覧(商品が存在しない場合はサイズ0件の商品一覧を返します)
+	 */
 	public List<Item> findAll() {
 		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items ORDER BY price_m;";
 		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
 		return itemList;
 	}
 
-	
+	/**
+	 * 商品一覧情報から商品名であいまい検索をし、値段の安い順で取得します.
+	 * 
+	 * @param name 商品名
+	 * @return 商品一覧(商品が存在しない場合はサイズ0件の商品一覧を返します)
+	 */
+	public List<Item> searchByNameContaining(String name) {
+		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items WHERE name LIKE :name ORDER BY price_m;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
+		List<Item> itemList = template.query(sql, param, ITEM_ROW_MAPPER);
+		return itemList;
+	}
 }
