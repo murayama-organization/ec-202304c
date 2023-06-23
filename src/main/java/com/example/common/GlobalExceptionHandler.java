38e7d15dcdead145d00e33d6744cae2222fb1830
tkeshun <shun9646@gmail.com>
2023-06-23 17:32:21 +0900

    500エラーでるようにした.カート追加ボタン押下時にログインしてないとカート一覧をみれないのを修正

--- src/main/java/com/example/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/common/GlobalExceptionHandler.java
@@ -2,31 +2,27 @@ package com.example.common;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.ModelAndView;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
- * アプリケーション内で処理されなかった例外をここでキャッチし、
- * エラーページへ遷移させます.
+ * アプリケーション内で処理されなかった例外をここでキャッチし、 エラーページへ遷移させます.
  * 
  * @author igamasayuki
  *
  */
-@Component
-public class GlobalExceptionHandler implements HandlerExceptionResolver {
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
 
 	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	/* (non-Javadoc)
-	 * @see org.springframework.web.servlet.HandlerExceptionResolver#resolveException(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.lang.Object, java.lang.Exception)
-	 */
-	@Override
-	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object obj, Exception e) {
-		logger.error("システムエラーが発生しました！", e);
-		return null;
+	@ExceptionHandler(Throwable.class)
+	public String handleConnectionError(Exception e) {
+
+		logger.error(e.getMessage(), e);
+
+		return "error/500";
 	}
+
 }
